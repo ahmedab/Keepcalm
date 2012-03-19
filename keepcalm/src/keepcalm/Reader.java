cd3cd3059a163f39aaf8e462cc8969434fde15f5
Davor Babic <davor@davor.se>
2012-03-19 14:26:49 +0100

    Wrote tests for Reader

--- keepcalm/src/keepcalm/Reader.java
+++ keepcalm/src/keepcalm/Reader.java
@@ -1,5 +1,16 @@
 package keepcalm;
 
-public class Reader {
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
+public class Reader {
+	private String fileName;
+	
+	public Reader(String fileName) throws FileNotFoundException {
+		this.fileName = fileName;
+	}
+	
+	public ArrayList<String> getInstance(int instanceNumber) {
+		return null;
+	}
 }
--- keepcalm/src/test/TestReader.java
+++ keepcalm/src/test/TestReader.java
@@ -1,10 +1,68 @@
 package test;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+import junit.framework.TestCase;
 
 import org.junit.Before;
+import static org.junit.Assert.*;
+import keepcalm.Reader;
 
-public class TestReader {
+public class TestReader extends TestCase {
+	private Reader r;
+	
 	@Before
 	public void setUp() throws Exception {
+		r = new Reader("testdata.csv");
+	}
+	
+	public void testGetInstance() {
+		assertEquals("getInstance(1) returns the wrong size!", 17, r.getInstance(1).size());
+		assertEquals("getInstance(60) returns the wrong size!", 17, r.getInstance(60).size());
+		assertEquals("getInstance(33) returns the wrong size!", 17, r.getInstance(33).size());
+	}
+	
+	/* had trouble making it work JUnit4-style for some
+	 * reason
+	 */
+	public void testGetInstanceThatDoesNotExist() {
+		try {
+			r.getInstance(99);
+			fail("Didn't throw NoSuchElementException.");
+		} catch (NoSuchElementException e) {
+			assertNotNull(e.getMessage());
+		}
+	}
+	
+	public void testNoSuchFile() {
+		try {
+			new Reader("idontexist.csv");
+			fail("Didn't throw FileNotFoundException.");
+		} catch (FileNotFoundException e) {
+			assertNotNull(e.getMessage());
+		}
+	}
+	
+	public void testReadInstance() {
+		ArrayList<String> d = r.getInstance(33);
+		assertEquals("High", d.get(1));
+		assertEquals("Low", d.get(2));
+		assertEquals("High", d.get(3));
+		assertEquals("Nominal", d.get(4));
+		assertEquals("Nominal", d.get(5));
+		assertEquals("Low", d.get(6));
+		assertEquals("Low", d.get(7));
+		assertEquals("Nominal", d.get(8));
+		assertEquals("Nominal", d.get(9));
+		assertEquals("Nominal", d.get(10));
+		assertEquals("Nominal", d.get(11));
+		assertEquals("High", d.get(12));
+		assertEquals("High", d.get(13));
+		assertEquals("Nominal", d.get(14));
+		assertEquals("Low", d.get(15));
+		assertEquals("66.6", d.get(16));
+		assertEquals("352.8", d.get(17));
 	}
 }
